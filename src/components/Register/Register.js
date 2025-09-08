1b8e8ae51e22a5dbf77d794b963e187ed1bfcdd5
Quoc Huu <nqhuund@gmail.com>
2025-09-08 22:33:54 +0700

    tao statate

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,6 +1,6 @@
 import { useHistory } from 'react-router-dom';
 import './Register.scss'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 
@@ -11,16 +11,42 @@ const Register = (props) => {
         history.push("/login")
     };
 
+    const [email, setmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
     useEffect(() => {
-        axios.get("https://reqres.in/api/users?page=2")
-            .then((response) => {
-                console.log("==>>>>>>>>>> check data", response.data);
-            })
-            .catch((error) => {
-                console.error("Lỗi gọi API:", error);
-            });
+        // axios.get("http://localhost:8686/api/user")
+        //     .then((response) => {
+        //         console.log("==>>>>>>>>>> check data", response);
+        //     })
+        //     .catch((error) => {
+        //         console.error("Lỗi gọi API:", error);
+        //     });
     }, []);
 
+    const validate = () => {
+
+        if (email.includes('@') === false) {
+            alert("Email không hợp lệ");
+            return;
+        };
+        if (!email || !phone || !username || !password || !confirmPassword) {
+            alert("Vui lòng điền đầy đủ thông tin");
+            return;
+        };
+        if (password !== confirmPassword) {
+            alert("Mật khẩu không khớp");
+            return;
+        };
+    }
+
+    const handleRegister = async () => {
+        validate();
+
+    }
     return (
         <div className="register-container">
             <div className="container">
@@ -39,25 +65,40 @@ const Register = (props) => {
                         </div>
                         <div className='form-group'>
                             <label>Email:</label>
-                            <input type="text" className="form-control" placeholder="Email address"></input>
+                            <input
+                                type="email" className="form-control" placeholder="Email address"
+                                value={email} onChange={(event) => setmail(event.target.value)}
+                            ></input>
                         </div>
                         <div className='form-group'>
                             <label>Phone number:</label>
-                            <input type="text" className="form-control" placeholder="Phone number"></input>
+                            <input
+                                type="text" className="form-control" placeholder="Phone number"
+                                value={phone} onChange={(event) => setPhone(event.target.value)}
+                            ></input>
                         </div>
                         <div className='form-group'>
                             <label>Usersname:</label>
-                            <input type="text" className="form-control" placeholder="Usersname"></input>
+                            <input
+                                type="text" className="form-control" placeholder="Usersname"
+                                value={username} onChange={(event) => setUsername(event.target.value)}
+                            ></input>
                         </div>
                         <div className='form-group'>
                             <label>Password:</label>
-                            <input type="password" className="form-control" placeholder="Password"></input>
+                            <input
+                                type="password" className="form-control" placeholder="Password"
+                                value={password} onChange={(event) => setPassword(event.target.value)}
+                            ></input>
                         </div>
                         <div className='form-group'>
                             <label>Re-enter Password:</label>
-                            <input type="password" className="form-control" placeholder="Re-enter Password"></input>
+                            <input
+                                type="password" className="form-control" placeholder="Re-enter Password"
+                                value={confirmPassword} onChange={(event) => setConfirmPassword(event.target.value)}
+                            ></input>
                         </div>
-                        <button className="btn btn-primary">Register</button>
+                        <button className="btn btn-primary" onClick={() => handleRegister()}>Register</button>
                         <hr />
                         <div className="text-center">
                             <button className="btn btn-success" onClick={() => handleLogin()}>
