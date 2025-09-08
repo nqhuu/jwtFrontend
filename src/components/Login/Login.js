9363ef3d41d696737cbbb71a44ceadd4d434f49c
huunq <nqhuund@gmail.com>
2025-09-08 11:26:07 +0700

    scss display login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,37 @@
+import './Login.scss'
+
 
 const Login = (props) => {
     return (
         <div className="login-container">
-            login
+            <div className="container">
+                <div className="row px-3 px-sm-0">
+                    <div className="content-left col-12 d-none col-sm-7 d-sm-block">
+                        <div className="brand">
+                            Giao diẹn login left
+                        </div>
+                        <div className="detail">
+                            Giao diẹn login left ...
+                        </div>
+                    </div>
+                    <div className="content-right col-12 col-sm-5 d-flex flex-column gap-3 py-3">
+                        <div className='brand d-sm-none'>
+                            displays login
+                        </div>
+                        <input type="text" className="form-control" placeholder="email address or phone number"></input>
+                        <input type="password" className="form-control" placeholder="Password"></input>
+                        <button className="btn btn-primary">Login</button>
+                        <span className="text-center">
+                            <a className="forgot-password" href="#">Forgotten your password?</a>
+                        </span>
+                        <hr />
+                        <div className="text-center">
+                            <button className="btn btn-success">Create new account</button>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
