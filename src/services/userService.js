73b0d3251e466ed342ee17caafe260419957ee1e
Quoc Huu <nqhuund@gmail.com>
2025-09-16 23:05:38 +0700

    add url create to userService

--- src/services/userService.js
+++ src/services/userService.js
@@ -8,6 +8,10 @@ const RegisterUser = async (formData) => {
     return await axios.post("http://localhost:8686/api/v1/register", { ...formData });
 }
 
+const createUser = async (formData) => {
+    return await axios.post("http://localhost:8686/api/v1/create", { ...formData });
+}
+
 const fetchAllUsers = async (limit, page) => {
     return await axios.get(`http://localhost:8686/api/v1/user/read/?limit=${limit}&page=${page}`);
 }
